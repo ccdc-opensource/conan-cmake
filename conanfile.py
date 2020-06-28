e5a3fcf6dbac5ec33e406a4b6bd56e834c92f0ba
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2020-06-28 21:46:02 +0300

    Disable static linking to openssl

--- conanfile.py
+++ conanfile.py
@@ -35,7 +35,7 @@ class CMakeConan(ConanFile):
             self._cmake = CMake(self)
             self._cmake.definitions["CMAKE_BOOTSTRAP"] = False
             if self.settings.os == "Linux":
-                self._cmake.definitions["OPENSSL_USE_STATIC_LIBS"] = True
+                self._cmake.definitions["OPENSSL_USE_STATIC_LIBS"] = False
                 self._cmake.definitions["CMAKE_EXE_LINKER_FLAGS"] = "-lz"
             self._cmake.configure()
         return self._cmake
